446acd8a28553a7cf3a83adda9ce1ba995d7d2de
Piotr Duda <piotrduda@MacBook-Pro-Piotr.local>
2020-11-13 13:16:51 +0100

    debug

--- assignment/settings.py
+++ assignment/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'assignment123123.herokuapp.com']
 
